aa8d046e947ac8c92055f19bcdc8a36d2a196966
Charlie W <cw5790@gmail.com>
2026-02-02 18:56:02 +0000

    feat: Implement static SVG flag mapping and refactor flag retrieval logic

--- src/components/FlagIcon/FlagIcon.tsx
+++ src/components/FlagIcon/FlagIcon.tsx
@@ -9,6 +9,7 @@ import React, { memo, useMemo } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { SvgXml } from 'react-native-svg';
 import { HOME_NATION_FLAGS } from './homeNationFlags';
+import { FLAG_SVG_MAP } from './flagSvgMap';
 
 export interface FlagIconProps {
   /** ISO 3166-1 alpha-2 code or GB subdivision (GB-ENG, GB-SCT, GB-WLS, GB-NIR) */
@@ -19,12 +20,7 @@ export interface FlagIconProps {
 }
 
 /**
- * Cache of loaded SVG strings to avoid repeated dynamic requires.
- */
-const svgCache = new Map<string, string | null>();
-
-/**
- * Load SVG string for a country code.
+ * Look up SVG string for a country code.
  * Returns null for unknown codes or bare 'GB'.
  */
 function getFlagSvg(code: string): string | null {
@@ -37,23 +33,8 @@ function getFlagSvg(code: string): string | null {
   const homeNation = HOME_NATION_FLAGS[upperCode];
   if (homeNation) return homeNation;
 
-  // Check cache
-  if (svgCache.has(upperCode)) {
-    return svgCache.get(upperCode) ?? null;
-  }
-
-  // Try to load from country-flag-icons
-  try {
-    // Dynamic require for the specific flag
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const flagModule = require(`country-flag-icons/string/3x2/${upperCode}`);
-    const svg = flagModule.default || flagModule;
-    svgCache.set(upperCode, svg);
-    return svg;
-  } catch {
-    svgCache.set(upperCode, null);
-    return null;
-  }
+  // Look up from static map
+  return FLAG_SVG_MAP[upperCode] ?? null;
 }
 
 function FlagIconComponent({ code, size = 16, testID }: FlagIconProps) {
---
+++ src/components/FlagIcon/flagSvgMap.ts
@@ -0,0 +1,183 @@
+/**
+ * Static map of ISO country codes to SVG strings from country-flag-icons.
+ *
+ * Metro bundler requires all imports to be statically analyzable,
+ * so we cannot use dynamic requires/imports. Each flag is imported individually.
+ */
+
+import AF from 'country-flag-icons/string/3x2/AF';
+import AL from 'country-flag-icons/string/3x2/AL';
+import DZ from 'country-flag-icons/string/3x2/DZ';
+import AD from 'country-flag-icons/string/3x2/AD';
+import AO from 'country-flag-icons/string/3x2/AO';
+import AR from 'country-flag-icons/string/3x2/AR';
+import AM from 'country-flag-icons/string/3x2/AM';
+import AU from 'country-flag-icons/string/3x2/AU';
+import AT from 'country-flag-icons/string/3x2/AT';
+import AZ from 'country-flag-icons/string/3x2/AZ';
+import BH from 'country-flag-icons/string/3x2/BH';
+import BD from 'country-flag-icons/string/3x2/BD';
+import BY from 'country-flag-icons/string/3x2/BY';
+import BE from 'country-flag-icons/string/3x2/BE';
+import BJ from 'country-flag-icons/string/3x2/BJ';
+import BO from 'country-flag-icons/string/3x2/BO';
+import BA from 'country-flag-icons/string/3x2/BA';
+import BR from 'country-flag-icons/string/3x2/BR';
+import BG from 'country-flag-icons/string/3x2/BG';
+import BF from 'country-flag-icons/string/3x2/BF';
+import BI from 'country-flag-icons/string/3x2/BI';
+import KH from 'country-flag-icons/string/3x2/KH';
+import CM from 'country-flag-icons/string/3x2/CM';
+import CA from 'country-flag-icons/string/3x2/CA';
+import CV from 'country-flag-icons/string/3x2/CV';
+import CF from 'country-flag-icons/string/3x2/CF';
+import TD from 'country-flag-icons/string/3x2/TD';
+import CL from 'country-flag-icons/string/3x2/CL';
+import CN from 'country-flag-icons/string/3x2/CN';
+import CO from 'country-flag-icons/string/3x2/CO';
+import KM from 'country-flag-icons/string/3x2/KM';
+import CG from 'country-flag-icons/string/3x2/CG';
+import CR from 'country-flag-icons/string/3x2/CR';
+import HR from 'country-flag-icons/string/3x2/HR';
+import CU from 'country-flag-icons/string/3x2/CU';
+import CY from 'country-flag-icons/string/3x2/CY';
+import CZ from 'country-flag-icons/string/3x2/CZ';
+import CD from 'country-flag-icons/string/3x2/CD';
+import DK from 'country-flag-icons/string/3x2/DK';
+import EC from 'country-flag-icons/string/3x2/EC';
+import EG from 'country-flag-icons/string/3x2/EG';
+import SV from 'country-flag-icons/string/3x2/SV';
+import GQ from 'country-flag-icons/string/3x2/GQ';
+import ER from 'country-flag-icons/string/3x2/ER';
+import EE from 'country-flag-icons/string/3x2/EE';
+import ET from 'country-flag-icons/string/3x2/ET';
+import FI from 'country-flag-icons/string/3x2/FI';
+import FR from 'country-flag-icons/string/3x2/FR';
+import GA from 'country-flag-icons/string/3x2/GA';
+import GM from 'country-flag-icons/string/3x2/GM';
+import GE from 'country-flag-icons/string/3x2/GE';
+import DE from 'country-flag-icons/string/3x2/DE';
+import GH from 'country-flag-icons/string/3x2/GH';
+import GR from 'country-flag-icons/string/3x2/GR';
+import GT from 'country-flag-icons/string/3x2/GT';
+import GN from 'country-flag-icons/string/3x2/GN';
+import GW from 'country-flag-icons/string/3x2/GW';
+import HT from 'country-flag-icons/string/3x2/HT';
+import HN from 'country-flag-icons/string/3x2/HN';
+import HU from 'country-flag-icons/string/3x2/HU';
+import IS from 'country-flag-icons/string/3x2/IS';
+import IN_FLAG from 'country-flag-icons/string/3x2/IN';
+import ID from 'country-flag-icons/string/3x2/ID';
+import IR from 'country-flag-icons/string/3x2/IR';
+import IQ from 'country-flag-icons/string/3x2/IQ';
+import IE from 'country-flag-icons/string/3x2/IE';
+import IL from 'country-flag-icons/string/3x2/IL';
+import IT from 'country-flag-icons/string/3x2/IT';
+import CI from 'country-flag-icons/string/3x2/CI';
+import JM from 'country-flag-icons/string/3x2/JM';
+import JP from 'country-flag-icons/string/3x2/JP';
+import JO from 'country-flag-icons/string/3x2/JO';
+import KZ from 'country-flag-icons/string/3x2/KZ';
+import KE from 'country-flag-icons/string/3x2/KE';
+import XK from 'country-flag-icons/string/3x2/XK';
+import KW from 'country-flag-icons/string/3x2/KW';
+import LV from 'country-flag-icons/string/3x2/LV';
+import LB from 'country-flag-icons/string/3x2/LB';
+import LR from 'country-flag-icons/string/3x2/LR';
+import LY from 'country-flag-icons/string/3x2/LY';
+import LT from 'country-flag-icons/string/3x2/LT';
+import LU from 'country-flag-icons/string/3x2/LU';
+import MG from 'country-flag-icons/string/3x2/MG';
+import MW from 'country-flag-icons/string/3x2/MW';
+import MY from 'country-flag-icons/string/3x2/MY';
+import ML from 'country-flag-icons/string/3x2/ML';
+import MT from 'country-flag-icons/string/3x2/MT';
+import MR from 'country-flag-icons/string/3x2/MR';
+import MX from 'country-flag-icons/string/3x2/MX';
+import MD from 'country-flag-icons/string/3x2/MD';
+import ME from 'country-flag-icons/string/3x2/ME';
+import MA from 'country-flag-icons/string/3x2/MA';
+import MZ from 'country-flag-icons/string/3x2/MZ';
+import NA from 'country-flag-icons/string/3x2/NA';
+import NL from 'country-flag-icons/string/3x2/NL';
+import NZ from 'country-flag-icons/string/3x2/NZ';
+import NI from 'country-flag-icons/string/3x2/NI';
+import NE from 'country-flag-icons/string/3x2/NE';
+import NG from 'country-flag-icons/string/3x2/NG';
+import KP from 'country-flag-icons/string/3x2/KP';
+import MK from 'country-flag-icons/string/3x2/MK';
+import NO from 'country-flag-icons/string/3x2/NO';
+import OM from 'country-flag-icons/string/3x2/OM';
+import PK from 'country-flag-icons/string/3x2/PK';
+import PS from 'country-flag-icons/string/3x2/PS';
+import PA from 'country-flag-icons/string/3x2/PA';
+import PY from 'country-flag-icons/string/3x2/PY';
+import PE from 'country-flag-icons/string/3x2/PE';
+import PH from 'country-flag-icons/string/3x2/PH';
+import PL from 'country-flag-icons/string/3x2/PL';
+import PT from 'country-flag-icons/string/3x2/PT';
+import QA from 'country-flag-icons/string/3x2/QA';
+import RO from 'country-flag-icons/string/3x2/RO';
+import RU from 'country-flag-icons/string/3x2/RU';
+import RW from 'country-flag-icons/string/3x2/RW';
+import SA from 'country-flag-icons/string/3x2/SA';
+import SN from 'country-flag-icons/string/3x2/SN';
+import RS from 'country-flag-icons/string/3x2/RS';
+import SL from 'country-flag-icons/string/3x2/SL';
+import SG from 'country-flag-icons/string/3x2/SG';
+import SK from 'country-flag-icons/string/3x2/SK';
+import SI from 'country-flag-icons/string/3x2/SI';
+import SO from 'country-flag-icons/string/3x2/SO';
+import ZA from 'country-flag-icons/string/3x2/ZA';
+import KR from 'country-flag-icons/string/3x2/KR';
+import SS from 'country-flag-icons/string/3x2/SS';
+import ES from 'country-flag-icons/string/3x2/ES';
+import SD from 'country-flag-icons/string/3x2/SD';
+import SE from 'country-flag-icons/string/3x2/SE';
+import CH from 'country-flag-icons/string/3x2/CH';
+import SY from 'country-flag-icons/string/3x2/SY';
+import TZ from 'country-flag-icons/string/3x2/TZ';
+import TH from 'country-flag-icons/string/3x2/TH';
+import TG from 'country-flag-icons/string/3x2/TG';
+import TT from 'country-flag-icons/string/3x2/TT';
+import TN from 'country-flag-icons/string/3x2/TN';
+import TR from 'country-flag-icons/string/3x2/TR';
+import UG from 'country-flag-icons/string/3x2/UG';
+import UA from 'country-flag-icons/string/3x2/UA';
+import AE from 'country-flag-icons/string/3x2/AE';
+import US from 'country-flag-icons/string/3x2/US';
+import UY from 'country-flag-icons/string/3x2/UY';
+import UZ from 'country-flag-icons/string/3x2/UZ';
+import VE from 'country-flag-icons/string/3x2/VE';
+import VN from 'country-flag-icons/string/3x2/VN';
+import YE from 'country-flag-icons/string/3x2/YE';
+import ZM from 'country-flag-icons/string/3x2/ZM';
+import ZW from 'country-flag-icons/string/3x2/ZW';
+
+export const FLAG_SVG_MAP: Record<string, string> = {
+  AD, AE, AF, AL, AM, AO, AR, AT, AU, AZ,
+  BA, BD, BE, BF, BG, BH, BI, BJ, BO, BR, BY,
+  CA, CD, CF, CG, CH, CI, CL, CM, CN, CO, CR, CU, CV, CY, CZ,
+  DE, DK, DZ,
+  EC, EE, EG, ER, ES, ET,
+  FI, FR,
+  GA, GE, GH, GM, GN, GQ, GR, GT, GW,
+  HN, HR, HT, HU,
+  ID, IE, IL, IN: IN_FLAG, IQ, IR, IS, IT,
+  JM, JO, JP,
+  KE, KH, KM, KP, KR, KW, KZ,
+  LB, LR, LT, LU, LV, LY,
+  MA, MD, ME, MG, MK, ML, MR, MT, MW, MX, MY, MZ,
+  NA, NE, NG, NI, NL, NO, NZ,
+  OM,
+  PA, PE, PH, PK, PL, PS, PT, PY,
+  QA,
+  RO, RS, RU, RW,
+  SA, SD, SE, SG, SI, SK, SL, SN, SO, SS, SV, SY,
+  TD, TG, TH, TN, TR, TT, TZ,
+  UA, UG, US, UY, UZ,
+  VE, VN,
+  XK,
+  YE,
+  ZA, ZM, ZW,
+};
